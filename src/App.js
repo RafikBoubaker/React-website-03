8f29b14522469ea6bff8530635a1eb6d626130bd
RafikBoubaker <boubaker.rafik@gmail.com>
2021-09-18 22:18:19 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Items from './Items';
+import Navbar from './Navbar';
+function App() {
+  return (
+    <div className="App">
+  <Navbar/>
+  <Items/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Items.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Items extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Items</h1>
+                <h2>0</h2>
+                <button>add</button>
+                <button>remove</button>
+            </div>
+        )
+    }
+}
